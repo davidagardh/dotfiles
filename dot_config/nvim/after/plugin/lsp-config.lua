71d2f6dab2f686781b20e0b5b87ab52d76e46751
David Agardh <agardh.david@gmail.com>
2023-03-29 17:30:31 +0200

    add go and clojure lsp

--- dot_config/nvim/after/plugin/lsp-config.lua
+++ dot_config/nvim/after/plugin/lsp-config.lua
@@ -1,6 +1,12 @@
 -- Typescript language server
 require'lspconfig'.tsserver.setup{}
+
 -- R language server
 require'lspconfig'.r_language_server.setup{
 	filetypes = {"r"}
 }
+
+require'lspconfig'.gopls.setup{}
+--
+-- Clojure lsp installed from https://github.com/clojure-lsp/clojure-lsp/releases
+require'lspconfig'.clojure_lsp.setup{}
