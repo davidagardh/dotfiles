962b77191c4c23c07fa22fd880323ef3ae9fc10d
David Agardh <agardh.david@gmail.com>
2023-04-03 18:08:37 +0200

    switch to kickstart-based config

--- dot_config/nvim/after/plugin/git.lua
+++
@@ -1,6 +0,0 @@
-require("neogit").setup {}
-
-local nnoremap = require "the0val.bind".nnoremap
-nnoremap("<leader>G", "<cmd>Neogit<CR>")
-
-require('gitsigns').setup()
--- dot_config/nvim/lua/the0val/init.lua
+++
@@ -1,3 +0,0 @@
-require "the0val.vim-opts"
-require "the0val.packer"
-require "the0val.keys"
