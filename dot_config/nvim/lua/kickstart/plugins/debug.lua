fe956fbab25d97fa2270aa72ed994cfe3e0aa487
David Agardh <agardh.david@gmail.com>
2023-04-04 23:40:41 +0200

    add scala config with metals and dap

--- dot_config/nvim/lua/kickstart/plugins/debug.lua
+++ dot_config/nvim/lua/kickstart/plugins/debug.lua
@@ -77,5 +77,26 @@ return {
 
     -- Install golang specific config
     require('dap-go').setup()
+
+    -- Scala specific config
+    dap.configurations.scala = {
+      {
+        type = "scala",
+        request = "launch",
+        name = "RunOrTest",
+        metals = {
+          runType = "runOrTestFile",
+          --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+        },
+      },
+      {
+        type = "scala",
+        request = "launch",
+        name = "Test Target",
+        metals = {
+          runType = "testTarget",
+        },
+      },
+    }
   end,
 }
