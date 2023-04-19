71111fa16f1978d6d14d1247993718fa2f5313ee
David Agardh <agardh.david@gmail.com>
2023-04-19 16:42:30 +0200

    fix bug for updated nvim-dap

--- dot_config/nvim/lua/kickstart/plugins/debug.lua
+++ dot_config/nvim/lua/kickstart/plugins/debug.lua
@@ -28,6 +28,10 @@ return {
       -- reasonable debug configurations
       automatic_setup = true,
 
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {},
+
       -- You'll need to check that you have the required things installed
       -- online, please don't ask me how to install them :)
       ensure_installed = {
@@ -36,10 +40,6 @@ return {
         'codelldb',
       },
     }
-
-    -- You can provide additional configuration to the handlers,
-    -- see mason-nvim-dap README for more information
-    require('mason-nvim-dap').setup_handlers()
 
     -- Basic debugging keymaps, feel free to change to your liking!
     vim.keymap.set('n', '<F5>', dap.continue)
