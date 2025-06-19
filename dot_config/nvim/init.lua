2a3c982badcc120cb52bfe8974c7ebf9f32348b9
David Agardh <agardh.david@gmail.com>
2025-06-19 11:18:27 +0200

    Set fish as shell in tmux and nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -31,6 +31,7 @@ require('lazy').setup({
       open_mapping = [[<M-t>]],
       insert_mappings = true,
       start_in_insert = true,
+      shell = 'fish',
       on_open = function(_)
         vim.cmd 'startinsert!'
       end,
