23aaf7aca92bfa19736235622fb6d931742a021b
David Agardh <agardh.david@gmail.com>
2026-04-08 10:39:40 +0200

    Add zoxide

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,3 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g tide_right_prompt_items status cmd_duration context jobs direnv node python rustc java php pulumi ruby go gcloud distrobox toolbox terraform aws nix_shell crystal elixir zig
 end
+
+if type -q zoxide
+    zoxide init fish | source
+end
